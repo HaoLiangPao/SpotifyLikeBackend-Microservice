b0b53288927d83e7be7db6a776f50ba46983f667
HaoLiangPao <hflsforeverhao@gmail.com>
2019-12-01 16:40:11 -0500

    GetSongById endpoint is completed for Song Microservice

--- starter_code/song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
+++ starter_code/song-microservice/src/main/java/com/csc301/songmicroservice/SongDalImpl.java
@@ -1,6 +1,7 @@
 package com.csc301.songmicroservice;
 
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import java.util.Hashtable;
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -57,7 +58,29 @@ public class SongDalImpl implements SongDal {
 	@Override
 	public DbQueryStatus findSongById(String songId) {
 		// TODO Auto-generated method stub
-		return null;
+		ObjectId objectId = new ObjectId(songId);
+		Hashtable queryPair = new Hashtable();
+		queryPair.put("_id", objectId);
+		Document query = new Document(queryPair);
+
+		MongoCursor<Document> cursor = collection.find(query).iterator();
+//		System.out.println("set is " + cursor.toString());
+		if (cursor.hasNext()){
+			Document songDocFound = cursor.next();
+			Song songFound = converter.toSong(songDocFound);
+
+			dbQueryStatus.setMessage("The song is successfully found in the database");
+			dbQueryStatus.setdbQueryExecResult(DbQueryExecResult.QUERY_OK);
+			dbQueryStatus.setData(songFound);
+		}
+		else {
+			//when object id is not existing int he database.
+			dbQueryStatus.setMessage("The song with id given is not found in the database");
+			dbQueryStatus.setdbQueryExecResult(DbQueryExecResult.QUERY_ERROR_NOT_FOUND);
+			dbQueryStatus.setData(null);
+		}
+		System.out.println(dbQueryStatus.getMessage());
+		return dbQueryStatus;
 	}
 
 	@Override
